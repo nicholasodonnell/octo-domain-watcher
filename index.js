ff8759662f6dee85237ce4bf1143f0274bf18837
Nicholas O'Donnell <nicholas@nicholasodonnell.com>
2022-11-01 01:23:43 -0400

    Refactor for simplicity

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const exit = require('exeunt')
+const { cond, equals, T } = require('ramda')
 const VError = require('verror')
 
-const all = require('./lib/all')
-const checkDomainForIssues = require('./lib/checkDomainForIssues')
+const checkDomainForIssue = require('./lib/checkDomainForIssue')
 const env = require('./lib/env')
-const handleResponseForIssues = require('./lib/handleResponseForIssues')
+const { isEmptyWhoisError, isInvalidDomainError } = require('./lib/errors')
+const handleDomainAvailableForIssue = require('./lib/handleDomainAvailableForIssue')
+const handleDomainUnavailableForIssue = require('./lib/handleDomainUnavailableForIssue')
+const handleInvalidDomainForIssue = require('./lib/handleInvalidDomainForIssue')
+const handleUnsupportedDomainForIssue = require('./lib/handleUnsupportedDomainForIssue')
 const logger = require('./lib/logger')
 const octokit = require('./lib/octokit')
 const { appSchema } = require('./lib/schema')
+
+// isTruthy :: Boolean -> Boolean
+const isAvailable = equals(true)
+
+// isFalsey :: Boolean -> Boolean
+const isUnavailable = equals(false)
+
+// logUnhandledError :: Error -> Void
+const logUnhandledErrorForIssue = issue => cause => {
+  const error = new VError({ cause, info: { issue } }, 'Unhandled error')
+
+  logger.error(error.message)
+}
 
 // app :: Object -> Void
 const app = async props => {
@@ -15,14 +32,21 @@ const app = async props => {
     logger.debug('Starting app with props', JSON.stringify(props))
 
     const { baseUrl, labels, repository, token } = await appSchema(props)
-
     const octo = octokit({ baseUrl, repository, token })
-
     const issues = await octo.fetchOpenIssues({ labels })
 
-    await checkDomainForIssues(issues)
-      .then(handleResponseForIssues(octo, issues))
-      .then(all)
+    for (const issue of issues) {
+      await checkDomainForIssue(issue)
+        .then(cond([
+          [ isAvailable,    handleDomainAvailableForIssue(octo, issue) ],
+          [ isUnavailable,  handleDomainUnavailableForIssue(octo, issue) ],
+        ]))
+        .catch(cond([
+          [ isInvalidDomainError, handleInvalidDomainForIssue(octo, issue) ],
+          [ isEmptyWhoisError,    handleUnsupportedDomainForIssue(octo, issue) ],
+          [ T,                    logUnhandledErrorForIssue(issue) ],
+        ]))
+    }
 
     logger.debug('Exiting app')
   } catch (e) {
